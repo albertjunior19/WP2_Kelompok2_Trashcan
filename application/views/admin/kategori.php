3b3ff1cb767e338d5a43ed243b5f59c2d5841ab5
indahputris <indahputrisnh@gmail.com>
2021-12-06 14:35:08 +0700

    mengubah

--- application/views/admin/kategori.php
+++ application/views/admin/kategori.php
@@ -9,6 +9,7 @@
             <tr>
               <th><em>No</em></th>
               <th><em>Nama Kategori</em></th>
+              <th><em>Url Kategori</em></th>
               <th><em>Opsi</em></th>
             </tr>
           </thead>
@@ -18,6 +19,7 @@
               <tr>
                 <td><?php echo $i . '.'; ?></td>
                 <td><span><?php echo $pro['kategori']; ?></span></td>
+                <td><i><?php echo $pro['url']; ?></i></td>
                 <td>
                 <a href="<?php echo base_url(); ?>admin/edit_kategori/<?php echo $pro['id_kategori']; ?>" class="btn-st drk-blu-clr"><i class="fa fa-edit"></i></a>
                   <a href="<?php echo base_url(); ?>admin/hapus_kategori/<?php echo $pro['id_kategori']; ?>" class="btn-st rd-clr bdel"><i class="fa fa-trash"></i></a>
